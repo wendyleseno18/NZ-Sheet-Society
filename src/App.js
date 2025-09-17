ef5a541a110efe429c62c794babca2c8f129bcf7
wendyleseno18 <wendyle.seno@yotpo.com>
2025-09-17 23:06:00 +0800

    Add the product and widget instance

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1213893";
+  let InstanceId_Widget = "1213910";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1213894";
+  let InstanceId_StarRating = "1213911";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "7211328634925";
+  let ProductId = "7646829019379";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/SKxx4qHr85n6q6jL4uaQwnnWQMmMC6EA2xWmkcGz" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/xhES5FXGiETGrfnMXjtBc0FCrPYDbNzm1XuQ79KI" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
